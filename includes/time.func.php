64f66ab6bf7a8dbb844355fbc4e40932c0cc5a8e
kevin.liu <791845283@qq.com>
2017-02-07 13:29:45 +0800

    修复时间问题

--- includes/time.func.php
+++ includes/time.func.php
@@ -2,12 +2,12 @@
 function get_open_time($time, $type='m-d'){
     $now = getdate($time);
     if ($now['wday'] >= 5 || $now['wday']  ==0 ){
-       $open = strtotime('Mon',$time);  //周一的那个时间点
-	}else{
-       $open = strtotime('+1 d',$time);  //明天的那个时间点
+        $open = strtotime('Mon',$time);    //周一的凌晨
+    }else{
+        $date1  = date("Y-m-d",$time);
+        $open   = strtotime("$date1 +1 day"); //明天的凌晨
+    }
 
-	}
-    $zero_time = strtotime(date("Y-m-d",$open));  //凌晨时间点
-    $open = $zero_time+3600*15;  //开奖时间是那天的三点
-	return $open;
+    $open = $open+3600*15;  //开奖时间是那天的三点
+    return $open;
 }
