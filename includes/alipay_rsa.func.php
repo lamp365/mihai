64cba02625c94a9ede79c9317f0bfb0502d1d62c
kevin.liu <791845283@qq.com>
2017-06-22 18:10:11 +0800

    阿里支付

--- includes/alipay_rsa.func.php
+++ includes/alipay_rsa.func.php
@@ -18,10 +18,10 @@
 function rsaSign($data, $private_key) {
     //以下为了初始化私钥，保证在您填写私钥时不管是带格式还是不带格式都可以通过验证。
     $private_key=str_replace("-----BEGIN RSA PRIVATE KEY-----","",$private_key);
-	$private_key=str_replace("-----END RSA PRIVATE KEY-----","",$private_key);
-	$private_key=str_replace("\n","",$private_key);
+    $private_key=str_replace("-----END RSA PRIVATE KEY-----","",$private_key);
+    $private_key=str_replace("\n","",$private_key);
 
-	$private_key="-----BEGIN RSA PRIVATE KEY-----".PHP_EOL .wordwrap($private_key, 64, "\n", true). PHP_EOL."-----END RSA PRIVATE KEY-----";
+    $private_key="-----BEGIN RSA PRIVATE KEY-----".PHP_EOL .wordwrap($private_key, 64, "\n", true). PHP_EOL."-----END RSA PRIVATE KEY-----";
 
     $res=openssl_get_privatekey($private_key);
 
@@ -34,7 +34,7 @@ function rsaSign($data, $private_key) {
         exit();
     }
     openssl_free_key($res);
-	//base64编码
+    //base64编码
     $sign = base64_encode($sign);
     return $sign;
 }
@@ -48,9 +48,9 @@ function rsaSign($data, $private_key) {
  */
 function  rsaVerify($data, $alipay_public_key, $sign)  {
     //以下为了初始化私钥，保证在您填写私钥时不管是带格式还是不带格式都可以通过验证。
-	$alipay_public_key=str_replace("-----BEGIN PUBLIC KEY-----","",$alipay_public_key);
-	$alipay_public_key=str_replace("-----END PUBLIC KEY-----","",$alipay_public_key);
-	$alipay_public_key=str_replace("\n","",$alipay_public_key);
+    $alipay_public_key=str_replace("-----BEGIN PUBLIC KEY-----","",$alipay_public_key);
+    $alipay_public_key=str_replace("-----END PUBLIC KEY-----","",$alipay_public_key);
+    $alipay_public_key=str_replace("\n","",$alipay_public_key);
 
     $alipay_public_key='-----BEGIN PUBLIC KEY-----'.PHP_EOL.wordwrap($alipay_public_key, 64, "\n", true) .PHP_EOL.'-----END PUBLIC KEY-----';
     $res=openssl_get_publickey($alipay_public_key);
@@ -62,8 +62,124 @@ function  rsaVerify($data, $alipay_public_key, $sign)  {
         echo "您的公钥格式不正确!"."<br/>"."The format of your alipay_public_key is incorrect!";
         exit();
     }
-    openssl_free_key($res);    
+    openssl_free_key($res);
     return $result;
+}
+
+/**
+ * 把数组所有元素，按照“参数=参数值”的模式用“&”字符拼接成字符串
+ * @param $para 需要拼接的数组
+ * return 拼接完成以后的字符串
+ */
+function alipay_createLinkstring($para){
+    $arg = "";
+    while (list ($key, $val) = each($para)) {
+        $arg.=$key . "=" . $val . "&";
+    }
+    $arg = substr($arg, 0, count($arg) - 2);
+    if (get_magic_quotes_gpc()) {
+        $arg = stripslashes($arg);
+    }
+    return $arg;
+}
+
+
+/**
+ * 把数组所有元素，按照“参数=参数值”的模式用“&”字符拼接成字符串，并对字符串做urlencode编码
+ * @param $para 需要拼接的数组
+ * return 拼接完成以后的字符串
+ */
+function alipay_createLinkstringUrlencode($para) {
+    $arg = "";
+    while (list ($key, $val) = each($para)) {
+        $arg.=$key . "=" . urlencode($val) . "&";
+    }
+    $arg = substr($arg, 0, count($arg) - 2);
+    if (get_magic_quotes_gpc()) {
+        $arg = stripslashes($arg);
+    }
+    return $arg;
+}
+
+/**
+ * 除去数组中的空值和签名参数
+ * @param $para 签名参数组
+ * return 去掉空值与签名参数后的新签名参数组
+ */
+function alipay_paraFilter($para) {
+    $para_filter = array();
+    while (list ($key, $val) = each($para)) {
+        if ($key == "sign" || $key == "sign_type" || $val == "")
+            continue;
+        else
+            $para_filter[$key] = $para[$key];
+    }
+    return $para_filter;
+}
+
+
+
+/**
+ * 远程获取数据，GET模式
+ * 注意：
+ * 1.使用Crul需要修改服务器中php.ini文件的设置，找到php_curl.dll去掉前面的";"就行了
+ * 2.文件夹中cacert.pem是SSL证书请保证其路径有效，目前默认路径是：getcwd().'\\cacert.pem'
+ * @param $url 指定URL完整路径地址
+ * @param $cacert_url 指定当前工作目录绝对路径
+ * return 远程输出的数据
+ */
+function alipay_getHttpResponseGET($url, $cacert_url) {
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_HEADER, 0); // 过滤HTTP头
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); // 显示输出结果
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true); //SSL证书认证
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2); //严格认证
+    curl_setopt($curl, CURLOPT_CAINFO, $cacert_url); //证书地址
+    $responseText = curl_exec($curl);
+    curl_close($curl);
+    return $responseText;
+}
+/**
+ * 实现多种字符编码方式
+ * @param $input 需要编码的字符串
+ * @param $_output_charset 输出的编码格式
+ * @param $_input_charset 输入的编码格式
+ * return 编码后的字符串
+ */
+function charsetEncode($input, $_output_charset, $_input_charset) {
+    $output = "";
+    if (!isset($_output_charset))
+        $_output_charset = $_input_charset;
+    if ($_input_charset == $_output_charset || $input == null) {
+        $output = $input;
+    } elseif (function_exists("mb_convert_encoding")) {
+        $output = mb_convert_encoding($input, $_output_charset, $_input_charset);
+    } elseif (function_exists("iconv")) {
+        $output = iconv($_input_charset, $_output_charset, $input);
+    } else
+        die("sorry, you have no libs support for charset change.");
+    return $output;
+}
+/**
+ * 实现多种字符解码方式
+ * @param $input 需要解码的字符串
+ * @param $_output_charset 输出的解码格式
+ * @param $_input_charset 输入的解码格式
+ * return 解码后的字符串
+ */
+function charsetDecode($input, $_input_charset, $_output_charset) {
+    $output = "";
+    if (!isset($_input_charset))
+        $_input_charset = $_input_charset;
+    if ($_input_charset == $_output_charset || $input == null) {
+        $output = $input;
+    } elseif (function_exists("mb_convert_encoding")) {
+        $output = mb_convert_encoding($input, $_output_charset, $_input_charset);
+    } elseif (function_exists("iconv")) {
+        $output = iconv($_input_charset, $_output_charset, $input);
+    } else
+        die("sorry, you have no libs support for charset changes.");
+    return $output;
 }
 
 ?>
---
+++ system/shopwap/class/mobile/alipay.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 刘建凡
+ * Date: 2017/6/22
+ * Time: 16:28
+ */
+namespace shopwap\controller;
+
+class alipay extends \common\controller\basecontroller
+{
+    /**
+     * 支付宝支付（即时到帐）
+     * @param string $ordersn 订单号
+     */
+    public function pay() {
+
+        $pay = new \service\shopwap\alipayService();
+        $result = $pay->alipay([
+//            'notify_url'  => WEBSITE_ROOT.'notify/alipay_notify.php',      //服务器异步通知页面路径
+            'notify_url'    => mobile_url('alipay',array('name'=>'shopwap','op'=>'notifyurl')),        //服务器异步通知页面路径
+//            'return_url'  => WEBSITE_ROOT.'notify/alipay_return_url.php', //页面跳转同步通知页面路径
+            'return_url'    => mobile_url('alipay',array('name'=>'shopwap','op'=>'returnurl')), //页面跳转同步通知页面路径
+            'out_trade_no'  => 'sn099239283879', //订单号
+            'subject'       => 'sn099239283879',  //标题
+            'total_fee'     => '0.01', //订单金额，单位为元
+            'body'          => str_replace("'", '‘', '测试商品'),
+            'show_url'      => WEBSITE_ROOT,  //商品展示地址 通过支付页面的表单进行传递
+        ]);
+        if (!$result) {
+            message($pay->getError());
+        }
+        die($result);
+    }
+
+    /**
+     * 服务器异步通知页面方法
+     */
+    function notifyurl()
+    {
+        $pay = new \service\shopwap\alipayService();
+        $result = $pay->notify_alipay();
+        if($result){
+            ajaxReturnData(1,'支付成功','success');
+        }else{
+            ajaxReturnData(0,'支付失败','fail');
+        }
+    }
+
+    /**
+     * 同步通知页面跳转处理方法
+     */
+    function returnurl()
+    {
+        $pay = new \service\shopwap\alipayService();
+        $result = $pay->return_alipay();
+        if($result) {
+            message('支付成功！',mobile_url('myorder',array('name'=>'shopwap')),'success');
+        } else {
+            message($pay->getError());
+        }
+    }
+
+}
