49b99b961cdc8f361c3322795767b6e8cee04f82
kevin.liu <791845283@qq.com>
2017-07-22 10:48:04 +0800

    subm

--- system/modules/class/web/payment_install.php
+++ system/modules/class/web/payment_install.php
@@ -1,28 +1,36 @@
 <?php
 
-			$code=$_GP['code'];
-			require WEB_ROOT.'/system/modules/plugin/payment/'.$code.'/lang.php';
-						
+$code=$_GP['code'];
+require WEB_ROOT.'/system/modules/plugin/payment/'.$code.'/lang.php';
 
-                 $item = mysqld_select("SELECT * FROM " . table('payment') . " WHERE code = :code", array(':code' => $code));
-              
-                 if (empty($item['id'])) {
-                 				 $data = array(
-                    'code' => $code,
-                    'name' => $_LANG['payment_'.$code.'_name'],
-                    'desc' => $_LANG['payment_'.$code.'_desc'],
-                    'enabled' => '0',
-                   'iscod' => $_LANG['payment_'.$code.'_iscod'],
-                   'online' => $_LANG['payment_'.$code.'_online']
-                  );
-									 mysqld_insert('payment', $data);
-                } else {
-                				 $data = array(
-                    'name' => $_LANG['payment_'.$code.'_name'],
-                    'desc' => $_LANG['payment_'.$code.'_desc'],
-                   'iscod' => $_LANG['payment_'.$code.'_iscod'],
-                   'online' => $_LANG['payment_'.$code.'_online']
-                  );
-                    mysqld_update('payment',$data , array('code' => $code));
-                }
-$this->do_payment_config();
+
+     $item = mysqld_select("SELECT * FROM " . table('payment') . " WHERE code = :code", array(':code' => $code));
+
+     if (empty($item['id'])) {
+                     $data = array(
+        'code' => $code,
+        'name' => $_LANG['payment_'.$code.'_name'],
+        'desc' => $_LANG['payment_'.$code.'_desc'],
+        'enabled' => '0',
+       'iscod' => $_LANG['payment_'.$code.'_iscod'],
+       'online' => $_LANG['payment_'.$code.'_online']
+      );
+                         mysqld_insert('payment', $data);
+    } else {
+                     $data = array(
+        'name' => $_LANG['payment_'.$code.'_name'],
+        'desc' => $_LANG['payment_'.$code.'_desc'],
+       'iscod' => $_LANG['payment_'.$code.'_iscod'],
+       'online' => $_LANG['payment_'.$code.'_online']
+      );
+        mysqld_update('payment',$data , array('code' => $code));
+    }
+$settings=globaSetting();
+if (checksubmit('submit')) {
+    require WEB_ROOT.'/system/modules/plugin/payment/'.$code.'/submit.php';
+
+    message('保存成功！',create_url('site', array('name' => 'modules','do' => 'payment','op'=>'list')),'success');
+}
+$item = mysqld_select("SELECT * FROM " . table('payment') . " WHERE code = :code", array(':code' => $code));
+$configs = unserialize($item['configs']);
+include WEB_ROOT.'/system/modules/plugin/payment/'.$code.'/config.php';
